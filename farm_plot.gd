4aa406a622d90cf5b144341ac3bc59e38385f7a1
John Miller <knp254@usask.ca>
2025-10-09 16:07:38 -0600

    Fix coordinate system for TM manager

--- farm_plot.gd
+++ farm_plot.gd
@@ -3,7 +3,7 @@ extends TileMapLayer
 var tm: TM_Manager
 
 var rows: int = 10
-var cols: int = 10
+var cols: int = 15
 
 enum {GRASS=0, DIRT=1, WET_DIRT=2}
 enum actions {TILL, WATER}
@@ -13,13 +13,13 @@ const state_machine: Dictionary = {
         Vector2i(DIRT, actions.WATER):WET_DIRT,
     }
 
-func _process(_delta: float):
+func _ready() -> void:
+    tm = TM_Manager.new(rows, cols, GRASS, state_machine)
     for i in range(rows):
         for j in range(cols):
             _cell_change(i, j)
 
-func _ready():
-    tm = TM_Manager.new(rows,cols, GRASS, state_machine)
+func _process(_delta: float) -> void:
     for i in range(rows):
         for j in range(cols):
             _cell_change(i, j)
@@ -27,9 +27,7 @@ func _ready():
 func _input(event) -> void:
     if event.is_action_pressed("click"):
         var a = local_to_map(event.get_position())
-        #print(a)
         tm.apply_transition(a.x,a.y,actions.TILL)
-        #_cell_change(a.x,a.y)
 
 func _cell_change(x: int, y: int) -> void:
     set_cell(Vector2i(x,y), 0, Vector2i(tm.get_tile(x,y), 0))
