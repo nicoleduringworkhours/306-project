fe5f3cda4047bc8581d5566d3e0548bcfd03deb1
John Miller <knp254@usask.ca>
2025-10-14 13:38:25 -0600

    formal documentation for tm_manager

--- farm_plot.gd
+++ farm_plot.gd
@@ -2,39 +2,47 @@ extends TileMapLayer
 
 # Model
 
-const rows: int = 20
-const cols: int = 10
+const rows: int = 20 ## rows in the tile map
+const cols: int = 10 ## columns in the tilemap
 
-enum {GRASS=0, DIRT=1, WET_DIRT=2}
-enum actions {TILL, WATER}
+enum {GRASS=0, DIRT=1, WET_DIRT=2} ## States of tiles
+enum actions {TILL, WATER} ## actions on tiles
 
+## non-timeout based states, of the form (state, action): state
 const state_machine: Dictionary = {
         Vector2i(GRASS, actions.TILL):DIRT,
         Vector2i(DIRT, actions.WATER):WET_DIRT,
     }
 
+## timeout based state transitions, of the form state: (state, time in seconds)
 const timer_states: Dictionary = {
         WET_DIRT: Vector2i(DIRT, 5)
     }
 
+## tile manager model
 var tm: TM_Manager
 
+## set-up. Creates a TM_Manager model, does initial renders of all tiles
+## actions on tiles
 func _ready() -> void:
-    tm = TM_Manager.new(rows, cols, GRASS, state_machine, timer_states)
+    tm = TM_Manager.new(cols, rows, GRASS, state_machine, timer_states)
     tm.cell_update.connect(_cell_update)
     for i in range(rows):
         for j in range(cols):
-            set_cell(Vector2i(i,j), 0, Vector2i(tm.get_tile(i,j), 0))
+            set_cell(Vector2i(j,i), 0, Vector2i(tm.get_tile(j,i), 0))
     add_child(tm.get_timer())
 
 # View
 
+## signal handler to deal with visual updates.
 func _cell_update(x: int, y: int, state) -> void:
     set_cell(Vector2i(x,y), 0, Vector2i(state, 0))
 
 # Controller
 
+## Temporary input handling.
 func _input(event) -> void:
+    # temporary input handling for testing.
     if event.is_action_pressed("click"):
         var a = local_to_map(event.get_position())
         tm.apply_transition(a.x,a.y,actions.TILL)
--- tm_manager.gd
+++ tm_manager.gd
@@ -1,26 +1,44 @@
 class_name TM_Manager
 extends RefCounted
 
-const TIMEOUT: float = 0.1
-const TIMEOUT_FACTOR: int = 10
+const TIMEOUT: int = 10 ## times/sec to check for timeout.
 
-var cols: int
-var rows: int
-var tile_data: Array[int] = []
+var cols: int ## number of columns (x-dimension)
+var rows: int ## number of rows (y-dimension)
+var tile_data: Array[int] = [] ## 1D array of tile data
 
-# (state: int, action: int): state:int
+## State machine dictating which actions change which states into
+## which others, entries must be of the form
+## (initial_state: int, action: int): destination_state: int
 var states: Dictionary
 
+## signals when a cell updates, transmits which cell changed, and what to
 signal cell_update(x: int, y: int, state: int)
 
+## timer for timeout states. must be added to scene tree with
+## get_timer if it will be used
 var timer: Timer
 
-# (state: int): (state:int, time: int)
+## State machine dictating the timeouts for states
+## if a cell transitions into a state, a timeout will be started, and
+## if the cell is still in the same state when the timeout completes,
+## it will automatically change to a different state
+## entries of the form (entry_state: int): (end_state:int, time_in_seconds: int)
 var timer_states: Dictionary
 
-# (x, y): (start_state, time_remaining)
+## the states currently in a timeout state
+## entries of the form (x: int, y: int): (start_state: int, time_remaining: int)
 var current_timers: Dictionary
 
+## initialize the tile manager
+## [param x] the number of columns for the tile map
+## [param y] the number of rows for the tile map
+## [param init_state] optional parameter for the default state of the
+##      cells, default is 0
+## [param state_machine] optional parameter for how states transition
+##      by action, default is no transitions. can be set later.
+## [param timer_state_machine] optional parameter for how states
+##      transition on timeout, default is no transitions, can be set later
 func _init(x: int, y: int, init_state: int = 0, state_machine: Dictionary = {}, timer_state_machine = {}) -> void:
     cols = x
     rows = y
@@ -33,8 +51,12 @@ func _init(x: int, y: int, init_state: int = 0, state_machine: Dictionary = {}, 
     timer.one_shot = false
     timer.autostart = true
     timer.timeout.connect(_handle_time)
-    timer.wait_time = TIMEOUT
+    timer.wait_time = 1.0/TIMEOUT
 
+## Apply an action to a cell
+## [param x] x coordinate of the cell
+## [param y] y coordinate of the cell
+## [param t] the action to apply
 func apply_transition(x: int, y: int, t: int):
     if x >= 0 and y >= 0 and x < cols and y < rows:
         var idx = x + y*cols
@@ -42,9 +64,10 @@ func apply_transition(x: int, y: int, t: int):
             var new_s: int = states[Vector2i(tile_data[idx], t)]
             tile_data[idx] = new_s
             if timer_states.has(new_s):
-                current_timers[Vector2i(x,y)] = Vector2i(new_s, timer_states[new_s].y* TIMEOUT_FACTOR)
+                current_timers[Vector2i(x,y)] = Vector2i(new_s, timer_states[new_s].y* TIMEOUT)
             cell_update.emit(x,y,tile_data[idx])
 
+## check for cell timeouts for any cells registered with a timeout.
 func _handle_time() -> void:
     for cell in current_timers.keys():
         if current_timers[cell].y == 0:
@@ -56,7 +79,8 @@ func _handle_time() -> void:
         if current_timers.has(cell):
             current_timers[cell] -= Vector2i(0,1)
 
-#setters/getters
+#setters/getters below
+
 func get_timer() -> Timer:
     return timer
 
