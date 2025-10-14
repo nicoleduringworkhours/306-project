5a1223437813569acf4b8da8e2bad25c3cd7c394
John Miller <knp254@usask.ca>
2025-10-14 12:55:39 -0600

    update TM_Manager with timeout support

--- farm_plot.gd
+++ farm_plot.gd
@@ -1,9 +1,9 @@
 extends TileMapLayer
 
-var tm: TM_Manager
+# Model
 
-var rows: int = 10
-var cols: int = 15
+const rows: int = 20
+const cols: int = 10
 
 enum {GRASS=0, DIRT=1, WET_DIRT=2}
 enum actions {TILL, WATER}
@@ -13,21 +13,32 @@ const state_machine: Dictionary = {
         Vector2i(DIRT, actions.WATER):WET_DIRT,
     }
 
-func _ready() -> void:
-    tm = TM_Manager.new(rows, cols, GRASS, state_machine)
-    for i in range(rows):
-        for j in range(cols):
-            _cell_change(i, j)
+const timer_states: Dictionary = {
+        WET_DIRT: Vector2i(DIRT, 5)
+    }
 
-func _process(_delta: float) -> void:
+var tm: TM_Manager
+
+func _ready() -> void:
+    tm = TM_Manager.new(rows, cols, GRASS, state_machine, timer_states)
+    tm.cell_update.connect(_cell_update)
     for i in range(rows):
         for j in range(cols):
-            _cell_change(i, j)
+            set_cell(Vector2i(i,j), 0, Vector2i(tm.get_tile(i,j), 0))
+    add_child(tm.get_timer())
+
+# View
+
+func _cell_update(x: int, y: int, state) -> void:
+    set_cell(Vector2i(x,y), 0, Vector2i(state, 0))
+
+# Controller
 
 func _input(event) -> void:
     if event.is_action_pressed("click"):
         var a = local_to_map(event.get_position())
         tm.apply_transition(a.x,a.y,actions.TILL)
 
-func _cell_change(x: int, y: int) -> void:
-    set_cell(Vector2i(x,y), 0, Vector2i(tm.get_tile(x,y), 0))
+    if event.is_action_pressed("hotkey_1"):
+        var a = local_to_map(get_viewport().get_mouse_position())
+        tm.apply_transition(a.x,a.y,actions.WATER)
--- tm_manager.gd
+++ tm_manager.gd
@@ -1,29 +1,70 @@
 class_name TM_Manager
 extends RefCounted
 
+const TIMEOUT: float = 0.1
+const TIMEOUT_FACTOR: int = 10
+
 var cols: int
 var rows: int
 var tile_data: Array[int] = []
+
+# (state: int, action: int): state:int
 var states: Dictionary
 
-func _init(x: int, y: int, init_state: int = 0, state_machine: Dictionary = {}) -> void:
+signal cell_update(x: int, y: int, state: int)
+
+var timer: Timer
+
+# (state: int): (state:int, time: int)
+var timer_states: Dictionary
+
+# (x, y): (start_state, time_remaining)
+var current_timers: Dictionary
+
+func _init(x: int, y: int, init_state: int = 0, state_machine: Dictionary = {}, timer_state_machine = {}) -> void:
     cols = x
     rows = y
     for a in range(cols * rows):
         tile_data.append(init_state)
     states = state_machine.duplicate()
+    timer_states = timer_state_machine.duplicate()
 
-func apply_transition(x: int, y: int, t: int) -> int:
+    timer = Timer.new()
+    timer.one_shot = false
+    timer.autostart = true
+    timer.timeout.connect(_handle_time)
+    timer.wait_time = TIMEOUT
+
+func apply_transition(x: int, y: int, t: int):
     if x >= 0 and y >= 0 and x < cols and y < rows:
         var idx = x + y*cols
         if states.has(Vector2i(tile_data[idx], t)):
-            tile_data[idx] = states[Vector2i(tile_data[idx], t)]
-        return tile_data[idx]
-    return -1
+            var new_s: int = states[Vector2i(tile_data[idx], t)]
+            tile_data[idx] = new_s
+            if timer_states.has(new_s):
+                current_timers[Vector2i(x,y)] = Vector2i(new_s, timer_states[new_s].y* TIMEOUT_FACTOR)
+            cell_update.emit(x,y,tile_data[idx])
+
+func _handle_time() -> void:
+    for cell in current_timers.keys():
+        if current_timers[cell].y == 0:
+            var idx: int = cell.x + cell.y * cols
+            if current_timers[cell].x == tile_data[idx]:
+                tile_data[idx] = timer_states[tile_data[idx]].x
+                cell_update.emit(cell.x,cell.y,tile_data[idx])
+            current_timers.erase(cell)
+        if current_timers.has(cell):
+            current_timers[cell] -= Vector2i(0,1)
 
 #setters/getters
+func get_timer() -> Timer:
+    return timer
+
 func set_state_machine(state_machine: Dictionary) -> void:
     states = state_machine.duplicate()
+
+func set_timer_state_machine(state_machine: Dictionary) -> void:
+    timer_states = state_machine.duplicate()
 
 func get_tile(x: int, y: int) -> int:
     if x >= 0 and y >= 0 and x < cols and y < rows:
