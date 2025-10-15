332faab0607a2d5407d0319459fbb7ec22a8eb6d
aqt824 <aqt824@usask.ca>
2025-10-14 18:29:28 -0600

    connected options and quit buttons to menu script

--- menu.gd
+++ menu.gd
@@ -3,3 +3,11 @@ extends Control
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://farm_plot.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
