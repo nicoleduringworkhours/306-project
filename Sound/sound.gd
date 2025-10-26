3854f517e0afe86624ffb2c5c45430f7a65cd910
John Miller <knp254@usask.ca>
2025-10-26 16:49:55 -0600

    acquire sfx

--- Sound/sound.gd
+++ Sound/sound.gd
@@ -12,11 +12,11 @@ enum BUS {MASTER, MUSIC, SFX}
 enum EFFECT {UI_CLICK, MENU, INTERACT, GROW, TOOL_SWAP}
 
 var sfx_lib: Dictionary = {
-        EFFECT.UI_CLICK: preload("res://Sound/rat.wav"),
-        EFFECT.MENU: preload("res://Sound/fire.wav"),
-        EFFECT.INTERACT: preload("res://Sound/fire.wav"),
-        EFFECT.GROW: preload("res://Sound/fire.wav"),
-        EFFECT.TOOL_SWAP: preload("res://Sound/fire.wav"),
+        EFFECT.UI_CLICK: preload("res://Sound/click.mp3"),
+        EFFECT.MENU: preload("res://Sound/menu.mp3"),
+        EFFECT.INTERACT: preload("res://Sound/dirt.mp3"),
+        EFFECT.GROW: preload("res://Sound/powerup.mp3"),
+        EFFECT.TOOL_SWAP: preload("res://Sound/chime.mp3"),
     }
 
 func _ready() -> void:
@@ -89,13 +89,3 @@ func get_vol(bus: BUS) -> float:
             v = sfx_vol
     return v
 
-# TODO: temp
-func _input(event):
-    if event.is_action_pressed("click"):
-        play_sfx(EFFECT.INTERACT)
-    if event.is_action_pressed("hotkey_1"):
-        play_sfx(EFFECT.UI_CLICK)
-    if event.is_action_pressed("hotkey_2"):
-        add_vol(BUS.MASTER, -0.05)
-    if event.is_action_pressed("hotkey_3"):
-        add_vol(BUS.MASTER, +0.05)
