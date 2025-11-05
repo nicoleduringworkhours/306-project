7f93a69c0497bfa0d1c7584a6fda1e812a325e60
aqt824 <aqt824@usask.ca>
2025-11-05 00:55:44 -0600

    attempting to make the options menu a child of main menu

--- Main-Menu/menu.gd
+++ Main-Menu/menu.gd
@@ -1,5 +1,8 @@
 extends Control
 
+@onready var options_menu = preload(res://Options/options.gd)
+var options_instance = null
+
 func _ready():
     $VBoxContainer/StartButton.grab_focus()
 
@@ -12,4 +15,15 @@ func _on_quit_button_pressed() -> void:
 
 
 func _on_options_button_pressed() -> void:
+    if options_instance == null :
+        options_instance = options_menu.instantiate()
+        add_child(options_instance)
+        
+        #connect to the close signal
+        options_instance.close_options.connect(close_options)
+        
+        #hide main and show options
+        hide_main_menu()
+        options_instance.show()
+    
     pass # Replace with function body.
