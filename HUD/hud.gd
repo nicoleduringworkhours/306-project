38a0b9898a4844ceb75a4d4316d6a09f3f3e82f1
aqt824 <aqt824@usask.ca>
2025-11-07 20:09:03 -0600

    implemented function get_money

--- HUD/hud.gd
+++ HUD/hud.gd
@@ -1,3 +1,13 @@
 extends Node
-func _get_money():
-    pass
+@onready var money = 0
+
+func _ready():
+    var tm = get_node("res://FarmPlot/tm_manager.gd")
+    tm.get_money.connect(_get_money)
+    
+    
+func _get_money(amount: int):
+    money += amount
+    
+    var money_label = get_node("res://FarmPlot/money.gd")
+    money_label.add_money(money)
