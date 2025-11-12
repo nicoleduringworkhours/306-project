3bbe0edfedcc6ed38d44f8349f9e086c893f2c90
John Miller <knp254@usask.ca>
2025-11-12 15:53:58 -0600

    refactor main menu

--- MainMenu/menu.gd
+++ MainMenu/menu.gd
@@ -1,45 +1,20 @@
 extends Control
 
-@onready var Options_Menu = preload("res://Options/options.tscn")
-var options_instance = null #keeps track of whether an options menu instance has been created
+@onready var opts = $Options
+@onready var vb = $VBoxContainer
 
 func _ready():
     #initial focus
     $VBoxContainer/StartButton.grab_focus()
+    opts.close_options.connect(option_toggle)
 
 func _on_start_button_pressed() -> void:
     get_tree().change_scene_to_file("res://GameScene.tscn")
 
-
 func _on_quit_button_pressed() -> void:
     get_tree().quit()
 
+func option_toggle() -> void:
+    vb.set_visible(not vb.visible)
+    opts.set_visible(not opts.visible)
 
-func _on_options_button_pressed() -> void:
-    if options_instance == null :
-        #create menu instance
-        options_instance = Options_Menu.instantiate() 
-        add_child(options_instance)
-        
-        #connect to the close signal and calls _on_options_closed() when it emits signal
-        options_instance.close_options.connect(_on_options_closed)
-        
-    #hide main and show options
-    hide_main_menu()
-    options_instance.show()
-    
-func _on_options_closed():
-    #show main menu buttons again
-    show_main_menu()
-    if options_instance:
-        #hide options menu
-        options_instance.hide()
-
-func hide_main_menu():
-    #hide main menu buttons
-    $VBoxContainer.hide()
-
-func show_main_menu():
-    #show main menu
-    $VBoxContainer.show()
-    
