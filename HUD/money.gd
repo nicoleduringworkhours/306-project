5d04c61d573fa483f45de664702763971f7283de
aqt824 <aqt824@usask.ca>
2025-11-27 17:20:08 -0600

    made money a global variable and added check in add_money

--- HUD/money.gd
+++ HUD/money.gd
@@ -3,10 +3,18 @@ extends Label
 var money: int = 50
 const text_pref: String = "MONEY: "
 
+func _ready() -> void:
+    text = text_pref + str(money)
+    
 func set_money(m: int) -> void:
     money = m
     text = text_pref + str(money)
 
 func add_money(m: int) -> void:
+    if money - m < 0:
+        return
     money += m
     text = text_pref + str(money)
+    
+func get_money()->int:
+    return money
