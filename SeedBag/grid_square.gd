614ae0f116106327f9cf97fb42489a9d077d8db3
RiverOTR (Joe) <mr.rivertimmothyotter@gmail.com>
2025-11-27 19:29:38 -0600

    Fix Broken Indexing of Crops in SeedBag

--- SeedBag/grid_square.gd
+++ SeedBag/grid_square.gd
@@ -2,15 +2,15 @@ class_name BagButton extends Button
 
 var button_crop: Crop.crop = Crop.crop.NONE
 var crop_sprite_lut = {
-    Crop.crop.BEAN: "res://Assets/SeedBag/potato.png",
-    Crop.crop.TOMATO: "res://Assets/SeedBag/tomato.png",
-    Crop.crop.EGGPLANT: "res://Assets/SeedBag/eggplant.png",
-    Crop.crop.PINEAPPLE: "res://Assets/SeedBag/pineapple.png",
-    Crop.crop.WHEAT: "res://Assets/SeedBag/wheat.png",
-    Crop.crop.STRAWBERRY: "res://Assets/SeedBag/strawberry.png",
-    Crop.crop.POTATO: "res://Assets/SeedBag/potato.png",
-    Crop.crop.ORANGE: "res://Assets/SeedBag/orange.png",
-    Crop.crop.CORN: "res://Assets/SeedBag/corn.png",
+    1: "res://Assets/SeedBag/bean.png",
+    2: "res://Assets/SeedBag/tomato.png",
+    3: "res://Assets/SeedBag/eggplant.png",
+    4: "res://Assets/SeedBag/pineapple.png",
+    5: "res://Assets/SeedBag/wheat.png",
+    6: "res://Assets/SeedBag/strawberry.png",
+    7: "res://Assets/SeedBag/potato.png",
+    8: "res://Assets/SeedBag/orange.png",
+    9: "res://Assets/SeedBag/corn.png",
 }
 
 signal modulate_button(button: BagButton)
@@ -23,6 +23,7 @@ func set_crop(crop: Crop.crop) -> void:
     disabled = crop == Crop.crop.NONE
     modulate = Color(1,1,1,1)
     button_crop = crop
-    var image = Image.load_from_file(crop_sprite_lut[crop])
-    var texture = ImageTexture.create_from_image(image)
-    icon = texture
+    if not disabled:
+        var image = Image.load_from_file(crop_sprite_lut[crop])
+        var texture = ImageTexture.create_from_image(image)
+        icon = texture
--- SeedBag/seed_bag.gd
+++ SeedBag/seed_bag.gd
@@ -6,11 +6,11 @@ var packed_gridsquare: PackedScene = preload("res://SeedBag/grid_square.tscn")
 var current_crop: Crop.crop = Crop.crop.CORN
 
 func _ready() -> void:
-    var crops = Crop.crop.keys()
+    var crop_keys = Crop.crop.keys()
     var n_crops = Crop.crop.size()
     for i in range(n_crops-1):
         %SeedGrid.add_child(packed_gridsquare.instantiate())
-        %SeedGrid.get_child(i).set_crop(Crop.crop[crops[i+1]])
+        %SeedGrid.get_child(i).set_crop(Crop.crop.get(crop_keys[i+1]))
         %SeedGrid.get_child(i).modulate_button.connect(_modulate_button)
     _modulate_button(%SeedGrid.get_child(0))
 
