6021ed5d2a06f5095e8c1577a4522a8e80043459
aqt824 <aqt824@usask.ca>
2025-11-27 22:45:34 -0600

    took out seed_planted signal and the function

--- FarmPlot/farm_plot.gd
+++ FarmPlot/farm_plot.gd
@@ -19,8 +19,10 @@ func _ready() -> void:
     add_child(_c)
 
     _c.harvested.connect(get_money.emit)
-    _c.seed_planted.connect(seed_planted)
     _p.got_watered.connect(_c.got_watered)
+    
+
+
 
 func hoe_press(loc: Vector2):
     _p.hoe_press(to_local(loc))
@@ -32,31 +34,7 @@ func shovel_press(loc: Vector2, s: Crop.crop):
 func water_press(loc: Vector2):
     _p.water_press(to_local(loc))
 
-func want_to_plant(crop_type: Crop.crop) -> bool:
-    match crop_type:
-         Crop.crop.CORN: 
-            cost_of_crop = 3
-            if Money.money - cost_of_crop <= 0:
-                return false
-         Crop.crop.WHEAT:
-             cost_of_crop = 2
-             if Money.money - cost_of_crop <= 0:
-                return false
-         Crop.crop.POTATO:
-            cost_of_crop = 5
-            if Money.money - cost_of_crop <= 0:
-                return false
-    return true
 
-## Emits a signal to get_money to deduct the total amount of money based on the 
-## crop type.
-
-## Parameters:
-## - crop_type: the kind of crop planted
- 
-func seed_planted(crop_type : Crop.crop):
-    if want_to_plant(crop_type) == true:
-        get_money.emit(-cost_of_crop)
     
         
         
