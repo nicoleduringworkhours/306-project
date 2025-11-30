1ee239299c643f0bc820dd5d846899b137f72fef
aqt824 <aqt824@usask.ca>
2025-11-30 00:12:49 -0600

    modified code to directly update global var money

--- HUD/money.gd
+++ HUD/money.gd
@@ -3,18 +3,10 @@ extends Label
 var money: int = 50
 const text_pref: String = "MONEY: "
 
-func _ready() -> void:
-    text = text_pref + str(money)
-    
 func set_money(m: int) -> void:
-    money = m
-    text = text_pref + str(money)
+    Money.money = m
+    text = text_pref + str(Money.money)
 
 func add_money(m: int) -> void:
-    if money - m <= 0:
-        return
-    money += m
-    text = text_pref + str(money)
-    
-func get_money()->int:
-    return money
+    Money.money += m
+    text = text_pref + str(Money.money)
