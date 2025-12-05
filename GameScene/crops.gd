f2341097c218d9e20bdf513838a5c774657b8fab
RiverOTR (Joe) <mr.rivertimmothyotter@gmail.com>
2025-12-05 16:40:53 -0600

    Add crop_unlock_cost and crop_texture Dictionaries to crops.gd

--- GameScene/crops.gd
+++ GameScene/crops.gd
@@ -1,4 +1,3 @@
-extends Node
 class_name Crop
 
 enum crop {
@@ -14,6 +13,7 @@ enum crop {
     CORN,
 }
 
+# FIX: not balanced
 const crop_val: Dictionary[crop, int] = {
     crop.NONE: 0,
     crop.BEAN: 1,
@@ -38,4 +38,30 @@ const crop_cost: Dictionary[crop, int] = {
     crop.POTATO: 5,
     crop.ORANGE: 3,
     crop.CORN: 6,
+}
+
+# FIX: not balanced
+const crop_unlock_cost: Dictionary[crop, int] = {
+    crop.NONE: 0,
+    crop.BEAN: 0,
+    crop.TOMATO: 1,
+    crop.EGGPLANT: 1,
+    crop.PINEAPPLE: 2,
+    crop.WHEAT: 3,
+    crop.STRAWBERRY: 5,
+    crop.POTATO: 8,
+    crop.ORANGE: 13,
+    crop.CORN: 21,
+}
+
+const crop_texture: Dictionary[crop, Resource] = {
+    crop.BEAN: preload("res://Assets/SeedBag/bean.png"),
+    crop.TOMATO: preload("res://Assets/SeedBag/tomato.png"),
+    crop.EGGPLANT: preload("res://Assets/SeedBag/eggplant.png"),
+    crop.PINEAPPLE: preload("res://Assets/SeedBag/pineapple.png"),
+    crop.WHEAT: preload("res://Assets/SeedBag/wheat.png"),
+    crop.STRAWBERRY: preload("res://Assets/SeedBag/strawberry.png"),
+    crop.POTATO: preload("res://Assets/SeedBag/potato.png"),
+    crop.ORANGE: preload("res://Assets/SeedBag/orange.png"),
+    crop.CORN: preload("res://Assets/SeedBag/corn.png"),
 }
