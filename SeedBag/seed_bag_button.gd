de5ba955741e8eb8c74c6c7f63c70627118b452c
RiverOTR (Joe) <mr.rivertimmothyotter@gmail.com>
2025-12-05 17:00:51 -0600

    Fix Incorrect Accessing of crop_unlock_cost in SeedBagButton

--- SeedBag/seed_bag_button.gd
+++ SeedBag/seed_bag_button.gd
@@ -5,21 +5,18 @@ var seed_bag: SeedBag
 
 func _on_seed_bag_updated() -> void:
     if seed_bag.seed_unlocked[crop]:
-        icon = Crop.crop_texture[c]
+        text = ""
+        icon = Crop.crop_texture[crop]
         _modulate_button(seed_bag.get_seed_state(crop))
     else:
-        text = "${0}".format(Crop.crop.crop_unlock_cost[crop])
+        text = "${0}".format(Crop.crop_unlock_cost[crop])
 
 func set_crop(c: Crop.crop) -> void:
     crop = c
     if !seed_bag.seed_unlocked[crop]:
-        text = "${0}".format(Crop.crop.crop_unlock_cost[crop])
+        text = "${0}".format(Crop.crop_unlock_cost[crop])
     else:
         icon = Crop.crop_texture[c]
-
-# TODO: implement
-func get_crop():
-    pass
 
 func set_seed_bag(sb: SeedBag) -> void:
     seed_bag = sb
