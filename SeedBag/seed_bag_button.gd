f0af57bd5ad1533ac55b865f641d7b6043b2566c
RiverOTR (Joe) <mr.rivertimmothyotter@gmail.com>
2025-12-05 16:44:42 -0600

    Add SeedBagView and SeedBagButton

---
+++ SeedBag/seed_bag_button.gd
@@ -0,0 +1,31 @@
+class_name SeedBagButton extends Button
+
+var crop: Crop.crop = Crop.crop.NONE
+var seed_bag: SeedBag
+
+func _on_seed_bag_updated() -> void:
+    if seed_bag.seed_unlocked[crop]:
+        icon = Crop.crop_texture[c]
+        _modulate_button(seed_bag.get_seed_state(crop))
+    else:
+        text = "${0}".format(Crop.crop.crop_unlock_cost[crop])
+
+func set_crop(c: Crop.crop) -> void:
+    crop = c
+    if !seed_bag.seed_unlocked[crop]:
+        text = "${0}".format(Crop.crop.crop_unlock_cost[crop])
+    else:
+        icon = Crop.crop_texture[c]
+
+# TODO: implement
+func get_crop():
+    pass
+
+func set_seed_bag(sb: SeedBag) -> void:
+    seed_bag = sb
+
+func _modulate_button(selected: bool) -> void:
+    if selected:
+        modulate = Color(1,1,0,1)
+    else:
+        modulate = Color(1,1,1,1)
---
+++ SeedBag/seed_bag_view.gd
@@ -0,0 +1,17 @@
+class_name SeedBagView extends Control
+
+const packed_seed_button: PackedScene = preload("res://SeedBag/seed_button.tscn")
+var n_crops = Crop.crop.size()-1 # -1 because we don't include Crop.crop.NONE
+
+func _ready() -> void:
+    var crop_keys = Crop.crop.keys()
+    for i in range(n_crops):
+        %SeedGrid.add_child(packed_seed_button.instantiate())
+        %SeedGrid.get_child(i).set_crop(Crop.crop.get(crop_keys[i+1])) # +1 to index 1-10 instead of 0-9
+
+func _tool_selected(tool: int) -> void:
+    visible = tool == 0
+
+func set_seed_bag(sb: SeedBag) -> void:
+    for i in range(n_crops):
+        %SeedGrid.get_child(i).set_seed_bag(sb)
